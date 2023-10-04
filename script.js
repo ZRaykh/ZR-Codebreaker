086eebc0aeca2dcf10035949d5f08683b8187129
zraykh <zraykh@gmail.com>
2023-10-04 15:32:38 -0400

    Finished base code

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function intitalize()
 {
     inputDisplay = document.getElementById("guess");
     guessesDisplay = document.getElementById("clock");
+    winlossDisplay = document.getElementById("win");
     reset();
     update();
 }
@@ -12,6 +13,7 @@ function reset()
     const secondNumber = Math.floor((Math.random() * 3) +1 );
     const thirdNumber = Math.floor((Math.random() * 3) +1 );
     code = firstNumber + "" +  secondNumber + "" + thirdNumber;
+    console.log(code)
     yourCode = "";
     turns = 7;
 }
@@ -31,32 +33,41 @@ function addTwo()
 function addThree()
 {
     yourCode = yourCode + "3";
+    console.log("Detected 3!");
     update();
 }
-function clear()
+
+function blank()
 {
     yourCode = "";
+    console.log("Detected Clear!");
     update();
 }
 
 function update()
 {
-    inputDisplay.innerHTML = "__ __ __ ";
+    inputDisplay.innerHTML = "_ _ _";
     if(yourCode.length == 1)
     {
-        inputDisplay.innerHTML = yourCode + "__ __";
+        inputDisplay.innerHTML = yourCode + " _ _";
     }
     if(yourCode.length == 2)
     {
-        inputDisplay.innerHTML = yourCode + "__";
+        inputDisplay.innerHTML = yourCode + " _";
     }
     if(yourCode.length == 3)
     {
         if (yourCode == code)
         {
-            
+            winlossDisplay.innerHTML = "Congradulations, you guessed the last code!";
+            reset();
         }
         turns = turns - 1;
+        if (turns < 1)
+        {
+            winlossDisplay.innerHTML = "You failed to guess the last code! Maybe you'll do better this time!";
+            reset();
+        }
         yourCode = "";
     }
     guessesDisplay.innerHTML = "Turns remaining: " + turns;
